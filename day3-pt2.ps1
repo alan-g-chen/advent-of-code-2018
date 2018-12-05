e74e8a3c935132874a152b8c2951173a4e32531b
Alan Chen <alanchen@microsoft.com>
2018-12-04 22:29:23 -0800

    Adding problem descriptions.

--- day3-pt2.ps1
+++ day3-pt2.ps1
@@ -1,4 +1,14 @@
-﻿# Some basic constraints - appears that the input is entirely in integer inches. So, we can use an N x M integer array to represent the fabric, 
+﻿<#
+
+Amidst the chaos, you notice that exactly one claim doesn't overlap by even a single square inch of fabric with any other claim. If you can somehow draw attention to it, maybe the Elves will be able to make Santa's suit after all!
+
+For example, in the claims above, only claim 3 is intact after all claims are made.
+
+What is the ID of the only claim that doesn't overlap?
+
+#>
+
+# Some basic constraints - appears that the input is entirely in integer inches. So, we can use an N x M integer array to represent the fabric, 
 # where the integer value represents # of claims.
 
 $claimsArray = New-Object int[][] 1000, 1000 # Intializes with a default value of 0
@@ -61,6 +71,7 @@ foreach ($item in $items)
     if ($claimIsValid) # Problem states that only a single claim is valid, so we can short circuit.
     {
         Write-Output "Found the valid claim! $item"
+        return
     }
     $claimIsValid = $true
 }
